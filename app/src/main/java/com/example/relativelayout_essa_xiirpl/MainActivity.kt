17b404bfc5a2b01d06aa7ad7c29ead311eb54bb9
Essa Rizky <essa.bogor2019@gmail.com>
2023-02-13 10:27:10 +0700

    Belajar Relative Layout

---
+++ app/src/main/java/com/example/relativelayout_essa_xiirpl/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.relativelayout_essa_xiirpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSpinnerDay()
+        setSpinnerTime()
+    }
+
+    private fun setSpinnerDay(){
+        val spinnerDay:Spinner=findViewById(R.id.spinner_date)
+
+        ArrayAdapter.createFromResource(
+            this,
+        R.array.day_array,
+            com.google.android.material.R.layout.support_simple_spinner_dropdown_item
+        ).also {
+            it.setDropDownViewResource(
+                androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item)
+            spinnerDay.adapter=it
+        }
+    }
+
+    private fun setSpinnerTime(){
+        val spinnerTime:Spinner=findViewById(R.id.spinner_time)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.time_array,
+            com.google.android.material.R.layout.support_simple_spinner_dropdown_item
+        ).also {
+            it.setDropDownViewResource(
+                androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item
+            )
+            spinnerTime.adapter = it
+        }
+    }
+
+}
